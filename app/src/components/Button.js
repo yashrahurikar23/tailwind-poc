a2370b39afa6e63b3edab5c6d78bf85ab343ed08
Yash Rahurikar <2yashrahurikar@gmail.com>
2020-07-09 23:59:24 +0530

    Created a button and input component

---
+++ app/src/components/Button.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from "react";
+
+function Button() {
+  return (
+    <Fragment>
+      <button>I am a button.</button>
+    </Fragment>
+  );
+}
+
+export default Button;
---
+++ app/src/components/Input.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from "react";
+
+function Input() {
+  return (
+    <Fragment>
+      <input placeholder="I am an input." />
+    </Fragment>
+  );
+}
+
+export default Input;
