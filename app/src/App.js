d0146f0a7bd5888108a6f471e35412ce536179c5
Yash Rahurikar <2yashrahurikar@gmail.com>
2020-07-09 23:57:01 +0530

    Added the base app structure for a login page

--- app/src/App.js
+++ app/src/App.js
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+// Packages
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+// Custom components
+import { LoginPage } from './container/Login';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Switch>
+          <Route path="/login" component={LoginPage} />
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ app/src/container/Login/Login.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from "react";
+
+function Login() {
+  return (
+    <Fragment>
+      <p>I am a login page.</p>
+    </Fragment>
+  );
+}
+
+export default Login;
